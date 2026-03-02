c750d0df57a00d5675bc7bbe2ba663b9616a1e2f
Jackson Sprenger <jsprenger@wisc.edu>
2026-03-02 22:57:39 +0000

    Revert "Added 3 new files, modified 3 files "
    This reverts commit ed4ec5a7376ff4aefc2f0168ba3b94b462de34b4.

--- test/controllers/documents_controller_test.rb
+++ test/controllers/documents_controller_test.rb
@@ -1,28 +1,8 @@
 require "test_helper"
 
 class DocumentsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @tenant = users(:tenant1)
-  end
-
-  test "redirects to login when not authenticated" do
-    get documents_path
-    assert_redirected_to login_path
-  end
-
-  test "renders index for logged in user" do
-    log_in_as(@tenant)
-
-    get documents_path
-    assert_response :success
-  end
-
-  test "create redirects when file is missing" do
-    log_in_as(@tenant)
-
-    post documents_path
-
-    assert_redirected_to new_document_path
-    assert_equal "Please choose a file to upload.", flash[:alert]
-  end
+  # test "should get index" do
+  #  get documents_index_url
+  #  assert_response :success
+  # end
 end
--- test/controllers/resources_controller_test.rb
+++ test/controllers/resources_controller_test.rb
@@ -1,27 +1,7 @@
 require "test_helper"
-require "fileutils"
 
 class ResourcesControllerTest < ActionDispatch::IntegrationTest
-  test "renders resources index" do
-    get resources_path
-    assert_response :success
-  end
-
-  test "renders resources even if faq files are missing" do
-    general_path = Rails.root.join("db", "faq_general.txt")
-    privacy_path = Rails.root.join("db", "faq_privacy.txt")
-    general_backup = Rails.root.join("db", "faq_general.txt.test_backup")
-    privacy_backup = Rails.root.join("db", "faq_privacy.txt.test_backup")
-
-    begin
-      FileUtils.mv(general_path, general_backup) if File.exist?(general_path)
-      FileUtils.mv(privacy_path, privacy_backup) if File.exist?(privacy_path)
-
-      get resources_path
-      assert_response :success
-    ensure
-      FileUtils.mv(general_backup, general_path) if File.exist?(general_backup)
-      FileUtils.mv(privacy_backup, privacy_path) if File.exist?(privacy_backup)
-    end
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end
