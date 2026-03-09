fb21f409f268979a8245983a35651ccc141b9b4f
Jackson Sprenger <jsprenger@wisc.edu>
2026-03-09 02:58:05 +0000

    Working architecture for running capybara tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,12 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+  # Container-only setup: browser runs in the chrome service and app runs in web service.
+  Capybara.run_server = false
+  Capybara.app_host = ENV.fetch("CAPYBARA_APP_HOST", "http://web:3000")
+
+  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ], options: {
+    browser: :remote,
+    url: ENV.fetch("SELENIUM_REMOTE_URL")
+  }
 end
