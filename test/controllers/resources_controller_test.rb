eadd483f91f5effe3f3f50c2827b44af4845de47
Jackson Sprenger <jsprenger@wisc.edu>
2026-03-13 20:43:42 +0000

    PR Resolution

--- test/controllers/resources_controller_test.rb
+++ test/controllers/resources_controller_test.rb
@@ -1,5 +1,5 @@
 require "test_helper"
-require "fileutils"
+require "minitest/mock" # Enable Minitest's `stub` helper in this test file
 
 class ResourcesControllerTest < ActionDispatch::IntegrationTest
   test "renders resources index" do
@@ -10,18 +10,21 @@ class ResourcesControllerTest < ActionDispatch::IntegrationTest
   test "renders resources even if faq files are missing" do
     general_path = Rails.root.join("db", "faq_general.txt")
     privacy_path = Rails.root.join("db", "faq_privacy.txt")
-    general_backup = Rails.root.join("db", "faq_general.txt.test_backup")
-    privacy_backup = Rails.root.join("db", "faq_privacy.txt.test_backup")
+    # Keep the real method for all non-FAQ paths during request rendering
+    original_exist = File.method(:exist?)
 
-    begin
-      FileUtils.mv(general_path, general_backup) if File.exist?(general_path)
-      FileUtils.mv(privacy_path, privacy_backup) if File.exist?(privacy_path)
-
+    File.stub(:exist?, ->(path) {
+      pathname = Pathname.new(path.to_s)
+      if pathname == general_path || pathname == privacy_path
+        # Simulate only these two files as missing
+        false
+      else
+        # Avoid affecting unrelated File.exist? checks in Rails/view rendering
+        original_exist.call(path)
+      end
+    }) do
       get resources_path
       assert_response :success
-    ensure
-      FileUtils.mv(general_backup, general_path) if File.exist?(general_backup)
-      FileUtils.mv(privacy_backup, privacy_path) if File.exist?(privacy_backup)
     end
   end
 end
