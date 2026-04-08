606aaecba7c152a7806cfa49e9b9587381293f7b
Jackson Sprenger <jsprenger@wisc.edu>
2026-04-08 17:35:06 +0000

    added tests for creating a user with invalid data

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -38,12 +38,54 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_equal "Account created successfully!", flash[:notice]
   end
 
-  test "re-renders the form when data is invalid" do
-    assert_no_difference("User.count") do
-      post signup_url, params: { user: { Email: "", password: "", ProfileDisclaimer: "no" } }
+  test "does not create a user with invalid data" do
+    invalid_params = {
+      Email: "not-an-email",
+      password: "",
+      password_confirmation: "",
+      FName: "",
+      LName: "",
+      Role: "Tenant",
+      TenantAddress: "",
+      ProfileDisclaimer: "no"
+    }
+
+    assert_no_enqueued_jobs do
+      assert_no_difference("User.count") do
+        post signup_url, params: { user: invalid_params }
+      end
     end
 
     assert_response :success
     assert_select "form"
+    assert_nil session[:user_id]
+    assert_nil flash[:notice]
+    assert_select ".error-text", text: /must be a valid email address/i
+    assert_select ".error-text", text: /you must agree to the disclaimer to sign up/i
+  end
+
+  test "does not create a user with duplicate email" do
+    duplicate_email_params = {
+      Email: "TEST@EXAMPLE.COM",
+      password: "Password!23",
+      password_confirmation: "Password!23",
+      FName: "Duplicate",
+      LName: "Tenant",
+      Role: "Tenant",
+      TenantAddress: "456 Broad St",
+      ProfileDisclaimer: "yes"
+    }
+
+    assert_no_enqueued_jobs do
+      assert_no_difference("User.count") do
+        post signup_url, params: { user: duplicate_email_params }
+      end
+    end
+
+    assert_response :success
+    assert_select "form"
+    assert_nil session[:user_id]
+    assert_nil flash[:notice]
+    assert_select ".error-text", text: /already registered/i
   end
 end
